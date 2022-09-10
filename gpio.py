43ce06dcccdbf36199d0e4e287aa3fef30147193
m.sadat <m.sadat@virasad.ir>
2022-09-10 12:43:24 +0000

    feat: test gpio for final scehmatic

--- gpio.py
+++ gpio.py
@@ -13,7 +13,7 @@ def handler(signal, frame):
 
 signal.signal(signal.SIGINT, handler)
 # Open GPIO 125 with input direction
-gpio07_0 = GPIO(65, "in")
+gpio07_0 = GPIO(56, "in")
 gpio16_1 = GPIO(101, "in")
 gpio18_2 = GPIO(121, "in")
 gpio19_3 = GPIO(4, "in")
@@ -32,10 +32,24 @@ gpio35_5 = GPIO(125, "out")
 gpio37_6 = GPIO(126, "out")
 
 while flag:
-  value = gpio07_0.read() + 2*gpio16_1.read() + 4*gpio18_2.read() + 8*gpio19_3.read() + 16*gpio21_4.read() + 32*gpio23_5.read() + 64*gpio24_6.read()
+  value = 1*gpio07_0.read() + 2*gpio16_1.read() + 4*gpio18_2.read() + 8*gpio19_3.read() + 16*gpio21_4.read() + 32*gpio23_5.read() + 64*gpio24_6.read()
   print(value)
-#  gpio_out.write(value)
-  time.sleep(1.0)
+  gpio27_0.write(gpio07_0.read())
+  gpio28_1.write(gpio16_1.read())
+  gpio29_2.write(gpio18_2.read())
+  gpio31_3.write(gpio19_3.read())
+  gpio33_4.write(gpio21_4.read())
+  gpio35_5.write(gpio23_5.read())
+  gpio37_6.write(gpio24_6.read())
+
+  gpio26_ext.write(False)
+
+  while(value != 0):
+    value = 1*gpio07_0.read() + 2*gpio16_1.read() + 4*gpio18_2.read() + 8*gpio19_3.read() + 16*gpio21_4.read() + 32*gpio23_5.read() + 64*gpio24_6.read()
+    print(value)
+    time.sleep(0.5)
+  gpio26_ext.write(True)
+  
 
 
 gpio_out.write(False)
