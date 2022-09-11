9065c68df6f25d5598aeaf3507a089255528d0fa
m.sadat <m.sadat@virasad.ir>
2022-09-11 10:11:23 +0000

    debug: decrease up to 32 from 64 input

--- gpio.py
+++ gpio.py
@@ -20,17 +20,18 @@ gpio19_3 = GPIO(4, "in")
 gpio21_4 = GPIO(3, "in")
 gpio23_5 = GPIO(2, "in")
 # Open GPIO 126 with output direction
-gpio24_ext = GPIO(6, "out")
-gpio24_ext.write(True)
+gpio26_ext = GPIO(6, "out")
+gpio26_ext.write(True)
 
 #gpio27_0 = GPIO(1, "out")
 #gpio28_1 = GPIO(0, "out")
-gpio26_0 = GPIO(5, "out")
-gpio29_1 = GPIO(122, "out")
-gpio31_2 = GPIO(123, "out")
-gpio33_3 = GPIO(124, "out")
-gpio35_4 = GPIO(125, "out")
-gpio37_5 = GPIO(126, "out")
+
+#gpio26_0 = GPIO(5, "out")
+gpio29_0 = GPIO(122, "out")
+gpio31_1 = GPIO(123, "out")
+gpio33_2 = GPIO(124, "out")
+gpio35_3 = GPIO(125, "out")
+gpio37_4 = GPIO(126, "out")
 
 def get_gpio_value():
   in_bit_0 = gpio07_0.read()
@@ -41,23 +42,23 @@ def get_gpio_value():
   in_bit_5 = gpio23_5.read()
   value = 1*in_bit_0 + 2*in_bit_1 + 4*in_bit_2 + 8*in_bit_3 + 16*in_bit_4 + 32*in_bit_5
   print(value)
-  gpio26_0.write(in_bit_0)
-  gpio29_1.write(in_bit_1)
-  gpio31_2.write(in_bit_2)
-  gpio33_3.write(in_bit_3)
-  gpio35_4.write(in_bit_4)
-  gpio37_5.write(in_bit_5)  
+
+  gpio29_0.write(in_bit_0)
+  gpio31_1.write(in_bit_1)
+  gpio33_2.write(in_bit_2)
+  gpio35_3.write(in_bit_3)
+  gpio37_4.write(in_bit_4)  
   return value
 
 while flag:
   value = get_gpio_value()
   if(value > 0):
     print("wait for arduino")
-    gpio24_ext.write(False)
-    time.sleep(1)
-    gpio24_ext.write(True)
-    time.sleep(1)
+    gpio26_ext.write(False)
+    time.sleep(0.5)
 
+    gpio26_ext.write(True)
+    time.sleep(0.5)
 
 gpio_out.write(False)
 gpio_in.close()
