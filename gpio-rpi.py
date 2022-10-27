85631111868ed831be8cc1b363e9b714fc1bbc1f
m.sadat <m.sadat@gmail.com>
2022-10-27 20:17:01 +0330

    debug: send count to server

--- gpio-rpi.py
+++ gpio-rpi.py
@@ -16,7 +16,7 @@ def watcher_update(register_id, quantity):
     }
     URL = "https://backend.monitait.com/api/factory/update-watcher/"
     r = requests.post(URL, data=json.dumps(DATA), headers=HEADER)
-    return r.json()
+    return r.status_code, r.json()
 
 flag = True
 
@@ -25,6 +25,7 @@ def handler(signal, frame):
   print('handler')
   flag = False
 
+i = 0 # iterator for send a dummy 0 request
 
 signal.signal(signal.SIGINT, handler)
 # Open GPIO 125 with input direction
@@ -36,7 +37,7 @@ gpio21_4 = GPIO(9, "in")
 gpio23_5 = GPIO(11, "in")
 # Open GPIO 126 with output direction
 gpio26_ext = GPIO(8, "out")
-gpio26_ext.write(True)
+gpio26_ext.write(False)
 
 #gpio27_0 = GPIO(1, "out")
 #gpio28_1 = GPIO(0, "out")
@@ -47,6 +48,12 @@ gpio31_1 = GPIO(6, "out")
 gpio33_2 = GPIO(13, "out")
 gpio35_3 = GPIO(19, "out")
 gpio37_4 = GPIO(26, "out")
+gpio29_0.write(False)
+gpio31_1.write(False)
+gpio33_2.write(False)
+gpio35_3.write(False)
+gpio37_4.write(False)
+
 
 def get_gpio_value():
   in_bit_0 = gpio07_0.read()
@@ -56,7 +63,7 @@ def get_gpio_value():
   in_bit_4 = gpio21_4.read()
   in_bit_5 = gpio23_5.read()
   value = 1*in_bit_0 + 2*in_bit_1 + 4*in_bit_2 + 8*in_bit_3 + 16*in_bit_4 + 32*in_bit_5
-  print(value)
+#  print(value)
 
   gpio29_0.write(in_bit_0)
   gpio31_1.write(in_bit_1)
@@ -66,16 +73,29 @@ def get_gpio_value():
   return value
 
 while flag:
-  value = get_gpio_value()
-  if(value > 0):
-    print("wait for arduino")
-    gpio26_ext.write(False)
-    time.sleep(0.5)
+  try:
+    value = get_gpio_value()
+    if(value > 0):
+      r_c, resp = watcher_update("1234567890", value)
+      print(r_c)
+      if r_c == requests.codes.ok:
+        print("send arduino: {}".format(value))
+        gpio26_ext.write(True)
+        time.sleep(0.5)
+        gpio26_ext.write(False)
+        i=0
+    else:
+      time.sleep(5)
+      i=i+1
+      if i > 12:
+        r_c, resp = watcher_update("1234567890", 0)
+        if r_c == requests.codes.ok:
+          i=0
 
-    gpio26_ext.write(True)
-    res = watcher_update("shyy2873gdj", value)
-    print(res)
-    time.sleep(0.5)
+  except Exception as e:
+    print("error: {}".format(str(e)))
+    time.sleep(2)
+    pass
 
 gpio_out.write(False)
 gpio_in.close()
