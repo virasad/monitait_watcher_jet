f73d81085a1c34eaaea5d3ed02d29a1754ed9ed3
m.sadat <asamasach@gmail.com>
2024-03-13 14:52:16 +0330

    fix: increase idle time

--- aux-python/init.sh
+++ aux-python/init.sh
@@ -3,7 +3,7 @@ python3 -m pip install python-periphery --break-system-packages
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "@reboot sleep 20; /usr/bin/python3 /home/pi/Shoga_monitait_watcher_jet/main.py &" >> mycron
+echo "@reboot sleep 60; /usr/bin/python3 /home/pi/Shoga_monitait_watcher_jet/main.py &" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
