0cc6c4126bae83bcabb968c769470db4bb249d89
m.sadat <asamasach@gmail.com>
2024-03-30 10:43:38 +0330

    feat: startup script for serial

--- aux-python/init.sh
+++ aux-python/init.sh
@@ -1,9 +1,11 @@
 sudo apt-get install python3-pip -y
 python3 -m pip install python-periphery --break-system-packages
+python3 -m pip install pyserial --break-system-packages
+python3 -m pip install pygame --break-system-packages
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "@reboot sleep 60; /usr/bin/python3 /home/pi/Shoga_monitait_watcher_jet/main.py &" >> mycron
+echo "@reboot sleep 60; /usr/bin/python3 /home/pi/Shoga_monitait_watcher_jet/main.py & ; /usr/bin/python3 /home/pi/Shoga_monitait_watcher_jet/main-serial.py &" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
