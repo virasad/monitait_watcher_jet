5ae63f705c551651e00be65a0d56c1be8c3f4bd2
m.sadat <asamasach@gmail.com>
2024-04-07 20:54:06 +0330

    debug: set requirements.txt absloute path

--- init/init.sh
+++ init/init.sh
@@ -1,6 +1,6 @@
 sudo apt-get install python3-pip -y
 sudo apt-get install git curl libsdl2-mixer-2.0-0 libsdl2-image-2.0-0 libsdl2-2.0-0 -y
-python3 -m pip install -r requirements.txt --break-system-packages
+python3 -m pip install -r /home/pi/monitait_watcher_jet/init/requirements.txt --break-system-packages
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
