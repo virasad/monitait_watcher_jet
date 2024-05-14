03adbcc29e9a0ea5bbcbb33d003cbab2acd996a3
arman <mr.kazemi.ar@gmail.com>
2024-05-14 14:46:11 +0330

    fix connection try catch

--- dev_main.py
+++ dev_main.py
@@ -12,13 +12,15 @@ import os
 import cv2
 from threading import Thread
 
-def watcher_update(register_id, quantity, defect_quantity, send_img, image_path="scene_image.jpg", product_id=0, lot_info=0, extra_info=None, *args, **kwargs):
+hostname = str(socket.gethostname())
+
+def watcher_update(register_id, quantity, defect_quantity, send_img, image_path="scene_image.jpg", product_id=0, lot_info=0, extra_info=None, timestamp=datetime.datetime.utcnow(), *args, **kwargs):
     quantity = quantity
     defect_quantity = defect_quantity
     product_id = product_id
     lot_info = lot_info
     extra_info = extra_info
-    timestamp = kwargs.pop("timestamp", datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f'))
+    timestamp = timestamp
     product_info = kwargs.pop("product_info", None)
 
     DATA = {
@@ -39,27 +41,37 @@ def watcher_update(register_id, quantity, defect_quantity, send_img, image_path=
     
     try:
         if send_img:
-            response = session.post(URL_DATA, data=json.dumps(DATA), headers={"content-type": "application/json"}, timeout=150)
-            result = response.json()
-            _id = result.get('_id', None)
-            time.sleep(1)
-            if _id:
-                DATA = {
-                    'register_id':result['register_id'],
-                    'elastic_id':_id
-                    }
-
-                response = session.post(URL_IMAGE, files={"image": open(image_path, "rb")}, data=DATA, timeout=250)
-                session.close()
-                return response.status_code
-            session.close
-            return response.status_code
+            try:
+                response = session.post(URL_DATA, data=json.dumps(DATA), headers={"content-type": "application/json"}, timeout=150)
+                result = response.json()
+                _id = result.get('_id', None)
+                time.sleep(1)
+                if _id:
+                    DATA = {
+                        'register_id':result['register_id'],
+                        'elastic_id':_id
+                        }
+                    try:
+                        response = session.post(URL_IMAGE, files={"image": open(image_path, "rb")}, data=DATA, timeout=250)
+                        session.close()
+                        return True
+                    except Exception as e:
+                        return True
+                session.close
+                return True
+            except Exception as e:
+                print(e)
+                return False
         else:
-            response = requests.post(URL, data=json.dumps(DATA), headers={"content-type": "application/json"})
-            return response.status_code
+            try:
+                response = requests.post(URL, data=json.dumps(DATA), headers={"content-type": "application/json"})
+                return True
+            except Exception as e:
+                print(e)
+                return False
     except Exception as e:
         session.close()
-        return requests.codes.bad 
+        return False
 
 class DB:
     def __init__(self) -> None:
@@ -74,7 +86,7 @@ class DB:
             print(e)
             return False
 
-    def write(self, register_id, a, b, extra_info, image_name, timestamp):
+    def write(self, register_id=hostname, a=0, b=0, extra_info={}, image_name="", timestamp=datetime.datetime.utcnow()):
         try:
             self.cursor.execute('''insert into monitait_table (register_id, temp_a, temp_b, timestamp, image_name, extra_info) values (?,?,?,?,?,?)''', (register_id, a, b, image_name, json.dumps(extra_info), timestamp))
             self.dbconnect.commit()
@@ -136,6 +148,30 @@ class Ardiuno:
         self.gpio26_d.write(True) # identify the default there is no read from RPI
         self.get_ts = 1
 
+    def open_serial(self):
+        try:
+            self.ser = serial.Serial(
+                    port='/dev/serial0', baudrate = 9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
+            self.serial_connection = True
+            self.serial_list = []
+            self.buffer = b''
+            self.last_received = ''
+            self.ser.flushInput()
+            self.serial_data = ""
+            return True
+        except Exception as e:
+            self.serial_connection = False
+            print(e)
+            return False
+
+    def close_serial(self):
+        try:
+            self.ser.close()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
     def int_to_bool_list(self, num):
         return [bool(num & (1<<n)) for n in range(4)]
 
@@ -146,54 +182,90 @@ class Ardiuno:
         self.gpio31_1.write(b_list[1])
         self.gpio29_0.write(b_list[0])
 
-    def run(self):
+    def run_serial(self):
+        while not self.stop_thread:
+            try:
+                tmp_seial_data = {}
+                buffer += self.ser.read(2000)
+                time.sleep(0.01)
+                if (b'\r\n' in buffer): # find line in serial data
+                    last_received, buffer = buffer.split(b'\r\n')[-2:]
+                    serial_list = str(last_received).split("'")[1].split(',')
+                    for z in range(len(serial_list)):
+                        tmp_seial_data.update({"d{}".format(z) : int(serial_list[z])})
+                    self.serial_data = tmp_seial_data
+                time.sleep(0.01)
+
+            except Exception as e:
+                if "Input/output" in str(e):
+                    try:
+                        self.close_serial()
+                        self.open_serial()
+                    except Exception as er:
+                        print(er)
+                if "fileno" in str(e):
+                    try:
+                        self.close_serial()
+                        self.open_serial()
+                    except Exception as ers:
+                        print(ers)
+                print(e)
+
+    def run_GPIO(self):
         self.old_start_ts = time.time()
         while not self.stop_thread:
-            in_bit_a = self.gpio21_a.read() # read arduino data
-            in_bit_b = self.gpio23_b.read()
-            in_bit_0 = self.gpio07_0.read()
-            in_bit_1 = self.gpio16_1.read()
-            in_bit_2 = self.gpio18_2.read()
-            in_bit_3 = self.gpio19_3.read()
+            try:
+                in_bit_a = self.gpio21_a.read() # read arduino data
+                in_bit_b = self.gpio23_b.read()
+                in_bit_0 = self.gpio07_0.read()
+                in_bit_1 = self.gpio16_1.read()
+                in_bit_2 = self.gpio18_2.read()
+                in_bit_3 = self.gpio19_3.read()
 
-            if in_bit_a and not(in_bit_b): # read arduino data a (OK)
-                a = 1*in_bit_0 + 2*in_bit_1 + 4*in_bit_2 + 8*in_bit_3
-            if (a > 0):
-                self.set_gpio_value(a)
-                self.gpio26_d.write(False)
-                while (self.gpio21_a.read() != self.gpio23_b.read()):
-                    time.sleep(0.001)
-                self.gpio26_d.write(True)
-                self.last_a += a
-                start_ts = time.time()
-                self.get_ts = 1/(start_ts - self.old_start_ts)+0.9*self.get_ts
-                self.old_start_ts = start_ts
+                if in_bit_a and not(in_bit_b): # read arduino data a (OK)
+                    a = 1*in_bit_0 + 2*in_bit_1 + 4*in_bit_2 + 8*in_bit_3
+                if (a > 0):
+                    self.set_gpio_value(a)
+                    self.gpio26_d.write(False)
+                    while (self.gpio21_a.read() != self.gpio23_b.read()):
+                        time.sleep(0.001)
+                    self.gpio26_d.write(True)
+                    self.last_a += a
+                    start_ts = time.time()
+                    self.get_ts = 1/(start_ts - self.old_start_ts)+0.9*self.get_ts
+                    self.old_start_ts = start_ts
 
-            elif not(in_bit_a) and in_bit_b: # read arduino data b (NG)
-                b = 1*in_bit_0 + 2*in_bit_1 + 4*in_bit_2 + 8*in_bit_3
-            if (b > 0):
-                self.set_gpio_value(b)
-                self.gpio37_c.write(False) # identify it is b
-                self.gpio26_d.write(False)
-                while (self.gpio21_a.read() != self.gpio23_b.read()):
-                    time.sleep(0.001)
-                self.gpio37_c.write(True) # identify default is a
-                self.gpio26_d.write(True)
-                self.last_b += b
-                start_ts = time.time()
-                self.get_ts = 1/(start_ts - self.old_start_ts)+0.9*self.get_ts
-                self.old_start_ts = start_ts
-            elif in_bit_a and in_bit_b: # read arduino data d (A7 in 15 levels [0..15])
-                self.d = 1*in_bit_0 + 2*in_bit_1 + 4*in_bit_2 + 8*in_bit_3
-            else:                       # read arduino data c (A5 in 15 levels [0..15])
-                self.c = 1*in_bit_0 + 2*in_bit_1 + 4*in_bit_2 + 8*in_bit_3
+                elif not(in_bit_a) and in_bit_b: # read arduino data b (NG)
+                    b = 1*in_bit_0 + 2*in_bit_1 + 4*in_bit_2 + 8*in_bit_3
+                if (b > 0):
+                    self.set_gpio_value(b)
+                    self.gpio37_c.write(False) # identify it is b
+                    self.gpio26_d.write(False)
+                    while (self.gpio21_a.read() != self.gpio23_b.read()):
+                        time.sleep(0.001)
+                    self.gpio37_c.write(True) # identify default is a
+                    self.gpio26_d.write(True)
+                    self.last_b += b
+                    start_ts = time.time()
+                    self.get_ts = 1/(start_ts - self.old_start_ts)+0.9*self.get_ts
+                    self.old_start_ts = start_ts
+                elif in_bit_a and in_bit_b: # read arduino data d (A7 in 15 levels [0..15])
+                    self.d = 1*in_bit_0 + 2*in_bit_1 + 4*in_bit_2 + 8*in_bit_3
+                else:                       # read arduino data c (A5 in 15 levels [0..15])
+                    self.c = 1*in_bit_0 + 2*in_bit_1 + 4*in_bit_2 + 8*in_bit_3
+                time.sleep(0.001)
+            except Exception as e:
+                print(e)
 
-    def read(self):
+    def read_GPIO(self):
         return self.last_a, self.last_b, self.c, self.get_ts
 
     def minus(self, a, b):
         self.last_a -= a
         self.last_b -= b
+
+    def read_serial(self):
+        return self.serial_data
 
 class Camera:
     def __init__(self, arduino, fps=30, exposure=100, gain=1, gamma=1, contrast=3, roi=[0,0,1920,1080], temperature=5000, brightness=1, step=10, auto_exposure=3) -> None:
@@ -262,7 +334,6 @@ class Camera:
         print('setuped')
         return video_cap
 
-
     def capture_and_save(self):
 
         if self.success:
@@ -293,8 +364,9 @@ class Counter:
             try:
                 data = self.db.read()
                 if len(data):
-                    ...
-
+                    if watcher_update(register_id=data[1], quantity=data[2], defect_quantity=data[3], send_img=len(data[4]) > 0, image_path=data[4], extra_info=json.loads(data[5]), timestamp=data[6]):
+                        self.db.delete(data[0])
+                time.sleep(1)
             except:
                 pass
 
@@ -302,7 +374,9 @@ class Counter:
         self.start_ts = time.time()
         while not self.stop_thread:
             try:
+                data_saved = False
                 send_image = False
+                extra_info = {}
                 ts = time.time()
                 a ,b ,c ,dps = self.arduino.read()
                 if a + b > dps or ts - self.start_ts > self.watcher_live_signal:
@@ -313,8 +387,15 @@ class Counter:
                             self.take_picture_interval = ts
                         else:
                             send_image = False
+                    extra_info = {}
                     timestamp = datetime.datetime.utcnow()
-
+                    if watcher_update(hostname, quantity=a, defect_quantity=b, send_img=send_image, image_path=image_name, extra_info=extra_info, timestamp=timestamp):
+                        data_saved = True
+                    else:
+                        if self.db.write(register_id=hostname, a=a, b=b, extra_info=extra_info, timestamp=timestamp, image_name=image_name):
+                            data_saved = True
+                    if data_saved:
+                        self.arduino.minus(a=a, b=b)
                 time.sleep(1)
             except Exception as e:
                 time.sleep(1)
