235fd8e204e9b77a137befb43bbe2099e45b8cea
m.sadat <asamasach@gmail.com>
2024-07-19 15:16:38 +0330

    test

--- main_gpio.py
+++ main_gpio.py
@@ -293,7 +293,7 @@ class Ardiuno:
                     self.c = 1*in_bit_0 + 2*in_bit_1 + 4*in_bit_2
                 
                 print(self.last_a, self.last_b, self.c, self.d)
-                self.gpio32_0.off()
+                self.gpio32_0.toggle()
                 time.sleep(0.01)
             except Exception as e:
                 print(f"arduino GPIO reader {e}")
