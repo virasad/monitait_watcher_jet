9ff26deaffd79b6593e9a4bf37f65b0611afe4d3
m.sadat <asamasach@gmail.com>
2024-09-17 18:16:14 +0330

    debug: fix serial encoder var and restart counting

--- main.py
+++ main.py
@@ -691,6 +691,9 @@ elif arduino.gpio18_0.value:
 else: 
     counter = Counter(arduino=arduino, db=db)
 
+if /dev/ttyUSB0:
+    scanner1= Scanner('/dev/ttyUSB0')
+
 Thread(target=counter.run).start()
 time.sleep(10)
 Thread(target=counter.db_checker).start()
