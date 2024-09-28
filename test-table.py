2c77e270b1701ce8ec625c1bd26d26517d3163c6
Akbar Sharghi <akbarsharghi1@gmail.com>
2024-09-28 13:21:24 +0330

    feat: added information to the title

--- test-table.py
+++ test-table.py
@@ -9,6 +9,8 @@ from PyQt5.QtCore import Qt
 # Your initial data
 data = (5, 'ZR2024092203', '[{"id": 163, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3096, "delivery_unit": "CAR", "product_name": "\\u0645\\u0627\\u06cc \\u0628\\u06cc\\u0628\\u06bc", "product_number": "20240819", "default_ids": ["1234567891111"], "description": "TEST", "batches": [{"quantity": "3096", "batch_uuid": "a8715fb9-7b72-4d28-b0c3-7723f43b7f6d", "assigned_id": "1234567891111"}], "status": "not_started", "batch_status": "view"}, {"id": 164, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3200, "delivery_unit": "CAR", "product_name": "\\u062f\\u0633\\u062a\\u0645\\u0627\\u0644 \\u0645\\u0631\\u0637\\u0648\\u0628", "product_number": "20240820", "default_ids": ["1234567892222"], "description": "TEST", "batches": [{"quantity": "3200", "batch_uuid": "bfc9d47a-ff2e-492e-9dd2-0eca1613dd68", "assigned_id": "1234567892222"}], "status": "not_started", "batch_status": "view"}, {"id": 165, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3300, "delivery_unit": "CAR", "product_name": "\\u06a9\\u0644\\u06cc\\u0646 \\u0627\\u067e", "product_number": "20240821", "default_ids": ["1234567893333"], "description": "TEST", "batches": [{"quantity": "3300", "batch_uuid": "f0c24da2-afe9-4cfa-9a94-ab72168d025a", "assigned_id": "1234567893333"}], "status": "not_started", "batch_status": "view"}]', 0)
 shipment_number = 'ZR2024092203'
+shipment_type = "ZP50"
+destination = "عجبشیر"
 # Parse the JSON string into a Python list of dictionaries
 json_data = json.loads(data[2])
 
@@ -20,7 +22,7 @@ class MainWindow(QMainWindow):
         self.setGeometry(100, 100, 800, 600)
 
         # Create a QLabel for the title
-        title_label = QLabel(f"جدول اطلاعات محصول، شماره محموله {shipment_number} ")
+        title_label = QLabel(f"جدول اطلاعات محصول، شماره محموله {shipment_number}، نوع محموله {shipment_type}، مقصد {destination} ")
         title_label.setFont(QFont("Arial", 20))  # Set font and size
         title_label.setStyleSheet("font-weight: bold; color: blue;")  # Style the title
 
