9821b19974974360e813a5f76e683a1a0f471f7a
Akbar Sharghi <akbarsharghi1@gmail.com>
2024-09-29 11:04:24 +0330

    feat: commented live stream image

--- test-table.py
+++ test-table.py
@@ -15,7 +15,7 @@ data = (5, 'ZR2024092203', '[{"id": 163, "start_date": "2019-08-24T14:15:22Z", "
 shipment_number = 'ZR2024092203'
 shipment_type = "ZP50"
 destination = "عجبشیر"
-url = "http://192.168.100.72:5000/video_feed/1"  # replace with your live stream URL
+live_stream_url = "http://192.168.100.72:5000/video_feed/1"  # replace with your live stream URL
 
 # Parse the JSON string into a Python list of dictionaries
 json_data = json.loads(data[2])
@@ -26,96 +26,85 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("اطلاعات محصول")
         self.setGeometry(100, 100, 800, 600)
-
-        # # Create a QLabel for the title
-        # Create a QTableWidget for the title
-        # Create a QTableWidget for the title
-        self.title_table = QTableWidget()  # No need to specify rows and columns at this point
-        self.title_table.setRowCount(2)  # Set 2 rows for 'a' and 'b'
-        self.title_table.setColumnCount(4)  # Set 1 column for values
         
         # Create a QFont for bold text
         bold_font = QFont()
         bold_font.setBold(True)
 
-        # Set the vertical header labels
-        # Set the vertical header labels
-        # Create and set values for columns 1 and 3
-        item_row1_col1 = QTableWidgetItem("شماره محموله")  # Row 1, Column 1
+        # Create a QTableWidget for the title
+        self.title_table = QTableWidget()  # No need to specify rows and columns at this point
+        self.title_table.setRowCount(2)  # Set 2 rows for 'a' and 'b'
+        self.title_table.setColumnCount(4)  # Set 1 column for values
         
-        item_row1_col1.setBackground(QColor("gray"))  # Set color for column 1
-        item_row1_col1.setFont(QFont("B Nazanin", 26))  # Set font and size
-        
-        item_row1_col2 = QTableWidgetItem(f"{shipment_number}")  # Row 1, Column 2 (empty)
+        # Create and set values 
+        item_row1_col1 = QTableWidgetItem("شماره محموله")  
+        item_row1_col1.setBackground(QColor("gray"))  
+        item_row1_col1.setFont(bold_font)
+        item_row1_col2 = QTableWidgetItem(f"{shipment_number}")  
         item_row1_col2.setFont(bold_font)
-        item_row1_col3 = QTableWidgetItem("نوع محموله")  # Row 1, Column 3
-        item_row1_col3.setBackground(QColor("gray"))  # Set color for column 1
-        item_row1_col3.setFont(QFont("B Nazanin", 26))  # Set font and size
-        item_row1_col4 = QTableWidgetItem(f"{shipment_type}")  # Row 1, Column 4 (empty)
+        
+        item_row1_col3 = QTableWidgetItem("نوع محموله")  
+        item_row1_col3.setBackground(QColor("gray"))  
+        item_row1_col3.setFont(bold_font)  
+        item_row1_col4 = QTableWidgetItem(f"{shipment_type}")  
         item_row1_col4.setFont(bold_font)
 
-        item_row2_col1 = QTableWidgetItem("مقصد")  # Row 2, Column 1
-        item_row2_col1.setFont(QFont("B Nazanin", 26))  # Set font and size
-        item_row2_col1.setBackground(QColor("gray"))  # Set color for column 1
-        item_row2_col2 = QTableWidgetItem(f"{destination}")  # Row 2, Column 2 (empty)
-        item_row2_col2.setFont(QFont("B Nazanin", 26))  # Set font and size
-        item_row2_col3 = QTableWidgetItem("مبدا")  # Row 2, Column 3
-        item_row2_col3.setBackground(QColor("gray"))  # Set color for column 1
-        item_row2_col3.setFont(QFont("B Nazanin", 26))  # Set font and size
-        item_row2_col4 = QTableWidgetItem("ساوه")  # Row 2, Column 4 (empty)
-        item_row2_col4.setFont(QFont("B Nazanin", 26))  # Set font and size
+        item_row2_col1 = QTableWidgetItem("مقصد")  
+        item_row2_col1.setFont(bold_font)    
+        item_row2_col1.setBackground(QColor("gray"))  
+        item_row2_col2 = QTableWidgetItem(f"{destination}")  
+        item_row2_col2.setFont(bold_font)  
+        
+        item_row2_col3 = QTableWidgetItem("مبدا")  
+        item_row2_col3.setBackground(QColor("gray"))  
+        item_row2_col3.setFont(bold_font)  
+        item_row2_col4 = QTableWidgetItem("ساوه")  
+        item_row2_col4.setFont(bold_font)  
                 
-
         # Set the stylesheet for the table to increase text size
         self.title_table.setStyleSheet("font-size: 25px;")  # Adjust size as needed
     
+        # Set values for the rows and columns
+        self.title_table.setItem(0, 0, item_row1_col1)  
+        self.title_table.setItem(0, 1, item_row1_col2)   
+        self.title_table.setItem(0, 2, item_row1_col3)   
+        self.title_table.setItem(0, 3, item_row1_col4)   
+        self.title_table.setItem(1, 0, item_row2_col1)  
+        self.title_table.setItem(1, 1, item_row2_col2)   
+        self.title_table.setItem(1, 2, item_row2_col3)   
+        self.title_table.setItem(1, 3, item_row2_col4)   
+        
+        # Set the column and rows width and height
+        self.title_table.setColumnWidth(0, 200)  
+        self.title_table.setColumnWidth(2, 200)  
+        self.title_table.setColumnWidth(1, 500)  
+        self.title_table.setColumnWidth(3, 500)  
+        self.title_table.setRowHeight(0, 100)  
+        self.title_table.setRowHeight(1, 100)  
 
-        # Set values for the rows labeled 'a' and 'b'
-        self.title_table.setItem(0, 0, item_row1_col1)  # 'a' row (index 0)
-        self.title_table.setItem(0, 1, item_row1_col2)   # 'b' row (index 1)
-        self.title_table.setItem(0, 2, item_row1_col3)   # 'b' row (index 1)
-        self.title_table.setItem(0, 3, item_row1_col4)   # 'b' row (index 1)
-        
-        self.title_table.setItem(1, 0, item_row2_col1)  # 'a' row (index 0)
-        self.title_table.setItem(1, 1, item_row2_col2)   # 'b' row (index 1)
-        self.title_table.setItem(1, 2, item_row2_col3)   # 'b' row (index 1)
-        self.title_table.setItem(1, 3, item_row2_col4)   # 'b' row (index 1)
-        
-        
-        self.title_table.setColumnWidth(0, 200)  # Set width for Column 1 (index 0)
-        self.title_table.setColumnWidth(2, 200)  # Set width for Column 3 (index 2)
-        self.title_table.setColumnWidth(1, 500)  # Set width for Column 1 (index 0)
-        self.title_table.setColumnWidth(3, 500)  # Set width for Column 3 (index 2)
-        
-        self.title_table.setRowHeight(0, 100)  # Set width for Column 1 (index 0)
-        self.title_table.setRowHeight(1, 100)  # Set width for Column 1 (index 0)
-        # self.title_table.item(0, 0).setBackground(QtGui.QColor(10,50,100))
-        # self.title_table.item(1, 0).setBackground(QtGui.QColor(10,50,100))
-        # self.title_table.item(2, 0).setBackground(QtGui.QColor(10,50,100))
-
-        
         # Set layout direction to right-to-left
         self.title_table.setLayoutDirection(Qt.RightToLeft)
 
-        # Optional: Make the header visible or set other properties as needed
+        # Make the header visible or set other properties as needed
         self.title_table.horizontalHeader().setVisible(False)  # Hide horizontal header if not needed
-        self.title_table.verticalHeader().setVisible(False)  # Hide horizontal header if not needed
+        self.title_table.verticalHeader().setVisible(False)  # Hide vertical header if not needed
         
-        # Create a QLabel for the image
-        self.image_label = QLabel(self)
-        
-
-        # Initialize live stream
-        
-        # self.timer = QTimer(self)
-        # self.timer.timeout.connect(self.update_frame)
-        # self.timer.start(30)  # Update every 30ms (~33 FPS)
+        # # Checking whether the live stream URL is alive or not
+        # try:
+        #     response = requests.head(live_stream_url, allow_redirects=True)
+        #     if response.status_code == 200: 
+        #         self.live_stream_flag = True
+        #     else:
+        #         self.live_stream_flag = False
+        # except:
+        #     self.live_stream_flag = True
+        #     pass
 
         self.table_widget = QTableWidget()
         self.table_widget.setColumnCount(6)
         self.table_widget.setLayoutDirection(Qt.RightToLeft)
         self.table_widget.setHorizontalHeaderLabels([
-            "شماره سفارش", "نام محصول", " شمارش شده", "مانده", "کل", "واحد تحویل"])
+            "شماره", "نام", " شمرده", "مانده", "کل", "تحویل"])
         # self.table_widget.horizontalHeader().setVisible(False)  # Hide horizontal header if not needed
         self.table_widget.verticalHeader().setVisible(False)  # Hide horizontal header if not needed
         
@@ -131,7 +120,12 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.title_table)  # Add title above the table
         layout.addWidget(self.table_widget)
         
-        layout.addWidget(self.image_label, alignment=Qt.AlignLeft | Qt.AlignTop)
+        # if self.live_stream_flag:
+        #     # Create a QLabel for the image
+        #     self.image_label = QLabel(self)
+        #     layout.addWidget(self.image_label, alignment=Qt.AlignLeft | Qt.AlignTop)
+        # else:
+        #     pass
         
         # Create a container widget to hold the layout and image
         container = QWidget()
@@ -148,29 +142,34 @@ class MainWindow(QMainWindow):
         self.timer = threading.Timer(1.0, self.update_table)
         self.timer.start()
     
-    def update_frame(self):
-        ret, frame = self.cap.read()
-        if ret:
-            # Convert frame to RGB
-            rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # Get the dimensions of the frame
-            h, w, ch = 250,680,3
-            print(h, w, ch)
-            # # Create QImage from the RGB frame
-            qimg = QImage(rgb_image.data, w, h, ch * w, QImage.Format_RGB888)
-            # # Set the QImage on the QLabel
-            self.image_label.setPixmap(QPixmap.fromImage(qimg))
+    # def update_frame(self):
+    #     ret, frame = self.cap.read()
+    #     if ret:
+    #         # Convert frame to RGB
+    #         rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #         # Get the dimensions of the frame
+    #         h, w, ch = rgb_image.shape
+    #         # # Create QImage from the RGB frame
+    #         qimg = QImage(rgb_image.data, w, h, ch * w, QImage.Format_RGB888)
+    #         # # Set the QImage on the QLabel
+    #         self.image_label.setPixmap(QPixmap.fromImage(qimg))
         
-    
-    def closeEvent(self, event):
-        self.cap.release()  # Release the video capture on close
-        event.accept()
+    # def closeEvent(self, event):
+    #     print(1)
+    #     if self.live_stream_flag:
+    #         self.cap.release()  # Release the video capture on close
+    #         event.accept()
+    #     else:
+    #         pass
+            
 
     def update_table(self):
         self.table_widget.setRowCount(0)  # Clear the table
-        
-        self.cap = cv2.VideoCapture(url)  # Capture from the default camera
-        self.update_frame()
+        # if self.live_stream_flag:
+        #     self.cap = cv2.VideoCapture(live_stream_url)  # Capture from the default camera
+        #     self.update_frame()
+        # else:
+        #     pass
         
         for item in json_data:
             row_position = self.table_widget.rowCount()
