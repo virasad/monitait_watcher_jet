a45e8d415a16f4b6e3b7b84c90d88a2c21764187
Akbar Sharghi <akbarsharghi1@gmail.com>
2024-09-29 19:27:34 +0330

    fix: removed additional print line

--- order_main_shipment.py
+++ order_main_shipment.py
@@ -20,14 +20,6 @@ shipment_url = 'https://develop-app.monitait.com/api/factory/shipment-orders/'
 stationID_url = f'https://develop-app.monitait.com/api/factory/watcher/{register_id}/'
 sendshipment_url = 'https://develop-app.monitait.com/api/elastic-search/send-batch-report/'
 
-data = (5, 'ZR2024092203', '[{"id": 163, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3096, "delivery_unit": "CAR", "product_name": "\\u0645\\u0627\\u06cc \\u0628\\u06cc\\u0628\\u06bc", "product_number": "20240819", "default_ids": ["1234567891111"], "description": "TEST", "batches": [{"quantity": "3096", "batch_uuid": "a8715fb9-7b72-4d28-b0c3-7723f43b7f6d", "assigned_id": "1234567891111"}], "status": "not_started", "batch_status": "view"}, {"id": 164, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3200, "delivery_unit": "CAR", "product_name": "\\u062f\\u0633\\u062a\\u0645\\u0627\\u0644 \\u0645\\u0631\\u0637\\u0648\\u0628", "product_number": "20240820", "default_ids": ["1234567892222"], "description": "TEST", "batches": [{"quantity": "3200", "batch_uuid": "bfc9d47a-ff2e-492e-9dd2-0eca1613dd68", "assigned_id": "1234567892222"}], "status": "not_started", "batch_status": "view"}, {"id": 165, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3300, "delivery_unit": "CAR", "product_name": "\\u06a9\\u0644\\u06cc\\u0646 \\u0627\\u067e", "product_number": "20240821", "default_ids": ["1234567893333"], "description": "TEST", "batches": [{"quantity": "3300", "batch_uuid": "f0c24da2-afe9-4cfa-9a94-ab72168d025a", "assigned_id": "1234567893333"}], "status": "not_started", "batch_status": "view"}]', 0)
-    
-json_data = json.loads(data[2])
-try:
-    os.system('./rm_py.sh')
-except Exception as ex0:
-    print(f"\n There are an error in openening the CPU version of watcher script, the error is {ex0}")  
-
 
 class MainWindow(QMainWindow):
     def __init__(self, arduino:Ardiuno, db:DB, camera:Camera, scanner, shipment_url: shipment_url, stationID_url: stationID_url,
@@ -220,7 +212,7 @@ class MainWindow(QMainWindow):
                                 pass
                             else:
                                 self.shipment_numbers_list.append(entry['shipment_number'])
-                        print("\n Time of adding shipment to DB", time.time() - s3, "self.shipment_numbers_list", self.shipment_numbers_listf)
+                        print("\n Time of adding shipment to DB", time.time() - s3, "self.shipment_numbers_list", self.shipment_numbers_list)
                     else:
                         pass
                 # except Exception as ex1:
