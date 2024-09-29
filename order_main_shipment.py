a390f4aa5fec16c9635a44cbebc88290d01ee229
Akbar Sharghi <akbarsharghi1@gmail.com>
2024-09-29 12:36:19 +0330

    fix: defined a variable

--- order_main_shipment.py
+++ order_main_shipment.py
@@ -357,6 +357,8 @@ except Exception as ex0:
             
 #             time.sleep(1)
 
+data = (5, 'ZR2024092203', '[{"id": 163, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3096, "delivery_unit": "CAR", "product_name": "\\u0645\\u0627\\u06cc \\u0628\\u06cc\\u0628\\u06bc", "product_number": "20240819", "default_ids": ["1234567891111"], "description": "TEST", "batches": [{"quantity": "3096", "batch_uuid": "a8715fb9-7b72-4d28-b0c3-7723f43b7f6d", "assigned_id": "1234567891111"}], "status": "not_started", "batch_status": "view"}, {"id": 164, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3200, "delivery_unit": "CAR", "product_name": "\\u062f\\u0633\\u062a\\u0645\\u0627\\u0644 \\u0645\\u0631\\u0637\\u0648\\u0628", "product_number": "20240820", "default_ids": ["1234567892222"], "description": "TEST", "batches": [{"quantity": "3200", "batch_uuid": "bfc9d47a-ff2e-492e-9dd2-0eca1613dd68", "assigned_id": "1234567892222"}], "status": "not_started", "batch_status": "view"}, {"id": 165, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3300, "delivery_unit": "CAR", "product_name": "\\u06a9\\u0644\\u06cc\\u0646 \\u0627\\u067e", "product_number": "20240821", "default_ids": ["1234567893333"], "description": "TEST", "batches": [{"quantity": "3300", "batch_uuid": "f0c24da2-afe9-4cfa-9a94-ab72168d025a", "assigned_id": "1234567893333"}], "status": "not_started", "batch_status": "view"}]', 0)
+json_data = json.loads(data[2])
 
 class MainWindow(QMainWindow):
     def __init__(self, arduino:Ardiuno, db:DB, camera:Camera, scanner, shipment_url: shipment_url, stationID_url: stationID_url,
@@ -390,8 +392,7 @@ class MainWindow(QMainWindow):
         self.take_picture_interval = 60 * 5
         self.order_db_remove_interval = 12 * 3600  # Convert hours to secends
         
-        self.json_data = (5, 'ZR2024092203', '[{"id": 163, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3096, "delivery_unit": "CAR", "product_name": "\\u0645\\u0627\\u06cc \\u0628\\u06cc\\u0628\\u06bc", "product_number": "20240819", "default_ids": ["1234567891111"], "description": "TEST", "batches": [{"quantity": "3096", "batch_uuid": "a8715fb9-7b72-4d28-b0c3-7723f43b7f6d", "assigned_id": "1234567891111"}], "status": "not_started", "batch_status": "view"}, {"id": 164, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3200, "delivery_unit": "CAR", "product_name": "\\u062f\\u0633\\u062a\\u0645\\u0627\\u0644 \\u0645\\u0631\\u0637\\u0648\\u0628", "product_number": "20240820", "default_ids": ["1234567892222"], "description": "TEST", "batches": [{"quantity": "3200", "batch_uuid": "bfc9d47a-ff2e-492e-9dd2-0eca1613dd68", "assigned_id": "1234567892222"}], "status": "not_started", "batch_status": "view"}, {"id": 165, "start_date": "2019-08-24T14:15:22Z", "delivery_date": "2019-08-24T14:15:22Z", "quantity": 3300, "delivery_unit": "CAR", "product_name": "\\u06a9\\u0644\\u06cc\\u0646 \\u0627\\u067e", "product_number": "20240821", "default_ids": ["1234567893333"], "description": "TEST", "batches": [{"quantity": "3300", "batch_uuid": "f0c24da2-afe9-4cfa-9a94-ab72168d025a", "assigned_id": "1234567893333"}], "status": "not_started", "batch_status": "view"}]', 0)
-
+        
         self.setWindowTitle("اطلاعات محصول")
         self.setGeometry(100, 100, 800, 600)
         
@@ -499,8 +500,8 @@ class MainWindow(QMainWindow):
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
-        self.previous_quantities = {item["id"]: item["quantity"] for item in json.loads(self.json_data[2])}
-        self.total_quantities = {item["id"]: item["quantity"] for item in json.loads(self.json_data[2])}
+        self.previous_quantities = {item["id"]: item["quantity"] for item in json_data}
+        self.total_quantities = {item["id"]: item["quantity"] for item in json_data}
 
         # Start the quantity decrease thread
         threading.Thread(target=self.decrease_quantity, daemon=True).start()
@@ -537,7 +538,7 @@ class MainWindow(QMainWindow):
         # else:
         #     pass
         
-        for item in json.loads(self.json_data[2]):
+        for item in json_data:
             row_position = self.table_widget.rowCount()
             self.table_widget.insertRow(row_position)
 
@@ -551,8 +552,8 @@ class MainWindow(QMainWindow):
             # Check if the quantity has decreased
             quantity_item = QTableWidgetItem(str(current_quantity))
             if current_quantity < previous_quantity:
+                print(12)
                 quantity_item.setBackground(QColor("red"))  # Highlight background in red
-            #  "شماره سفارش", "نام محصول", " شمارش شده", "مانده", "کل", "واحد تحویل"
             # Add items to the table
             self.table_widget.setItem(row_position, 0, QTableWidgetItem(str(item["id"])))
             self.table_widget.setItem(row_position, 1, QTableWidgetItem(product_name))
@@ -563,7 +564,7 @@ class MainWindow(QMainWindow):
             
 
         # Update previous quantities for the next iteration
-        for item in json.loads(self.json_data[2]):
+        for item in json_data:
             self.previous_quantities[item["id"]] = item["quantity"]
 
         # Restart the timer
@@ -572,7 +573,8 @@ class MainWindow(QMainWindow):
 
     def decrease_quantity(self):
         while True:
-            for item in json.loads(self.json_data[2]):
+            for item in json_data:
+                print("item['quantity']", item["quantity"])
                 if item["quantity"] > 0:
                     item["quantity"] -= 1  # Decrease the quantity by 100
             time.sleep(2)  # Wait for 1 second before the next decrease
