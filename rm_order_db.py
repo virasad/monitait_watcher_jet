bee081aa1af598195a5bf381be70a28f5553da68
Akbar Sharghi <akbarsharghi1@gmail.com>
2024-09-29 18:46:39 +0330

    removing order db

---
+++ rm_order_db.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+dbconnect = sqlite3.connect("/home/pi/monitait_watcher_jet/monitait.db", check_same_thread=False)
+cursor = dbconnect.cursor()
+
+
+
+cursor.execute("""DELETE from watcher_order_table""")
+dbconnect.commit()
