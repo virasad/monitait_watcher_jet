c04fb9813577cd26f4025773f9dbe418eb054b3d
Akbar Sharghi <akbarsharghi1@gmail.com>
2024-10-16 19:50:26 +0330

    feat: changed column size mode

--- order_main_shipment.py
+++ order_main_shipment.py
@@ -105,10 +105,10 @@ class MainWindow(QMainWindow):
         header = self.table_widget.horizontalHeader()
         header.setSectionResizeMode(QHeaderView.Stretch)
         
-        header.setResizeMode(0, QtGui.QHeaderView.Stretch)
-        header.setResizeMode(1, QtGui.QHeaderView.ResizeToContents)
-        header.setResizeMode(2, QtGui.QHeaderView.ResizeToContents)
-        header.setResizeMode(3, QtGui.QHeaderView.Stretch)
+        header.setSectionResizeMode(0, QtGui.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtGui.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(2, QtGui.QHeaderView.ResizeToContents)
+        header.setSectionResizeMode(3, QtGui.QHeaderView.Stretch)
 
         # Create a layout to arrange the title and table
         layout = QVBoxLayout()
