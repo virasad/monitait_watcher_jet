4d530056cdad6e5539f637195ca12867086969ed
Akbar Sharghi <akbarsharghi1@gmail.com>
2024-10-31 16:02:33 +0330

    fix: removed additional function

--- rm_order_db.py
+++ rm_order_db.py
@@ -3,16 +3,16 @@ from utils.base_s import *
 
 dbconnect = sqlite3.connect("/home/pi/monitait_watcher_jet/monitait.db", check_same_thread=False)
 
-cursor2 = self.dbconnect.cursor()
-if status == "total": 
-      cursor2.execute('SELECT * FROM shipment_table')
-      rows = cursor2.fetchall()
-      if len(rows) == 0:
-            cursor2.close()
-      else:
-            cursor2.close()
+cursor2 = dbconnect.cursor()
 
+cursor2.execute('SELECT * FROM shipment_table')
+rows = cursor2.fetchall()
+if len(rows) == 0:
       cursor2.close()
+else:
+      cursor2.close()
+
+cursor2.close()
 
 print(rows, "Rows")
 
